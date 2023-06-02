9c60d1648ddc3e2fa326f6ce2c7e3646c614bf5d
LucsCarl <LucsCarl@broMomento>
2023-06-02 12:19:54 -0300

    Comentarios opcion

--- Entidades/OpcionLlamada.cs
+++ Entidades/OpcionLlamada.cs
@@ -22,11 +22,16 @@ namespace PPAI_IVR_2023.Entidades
             this.validacionesRequeridas = validacionesRequeridas;
         }
 
+        /// <summary> Comprueba que la opcion sea la misma </summary>
+        /// <param name="opcion"> Opcion que se desea comparar </param>
         public bool EsOpcion(OpcionLlamada opcion)
         {
             return opcion == this;
         }
 
+        /// <summary> Pregunta si la opcion tiene la subopcion deseada </summary>
+        /// <param name="subOpcion"> Subopcion que se desea buscar </param>
+        /// <returns> Indice de la subopcion en la lista de subopciones de la opcion. Devuelve -1 si no se encuentra </returns>
         public int ContieneSubOpcion(SubOpcionLlamada subOpcion)
         {
             //Comprueba que tenga subOpciones
@@ -42,17 +47,21 @@ namespace PPAI_IVR_2023.Entidades
             return -1;
         }
 
+        /// <summary> Obtiene el nombre de la subopcion deseada </summary>
+        /// <param name="subop"> Indice de la subopcion </param>
         public string ObtenerNombreSubOpcion(int subop)
         {
             return subOpciones[subop].ObtenerNombreSubOpcion();
         }
 
+        /// <summary> Obtiene el nombre de la opcion, sumando su nroOrden y su nombre </summary>
         public string ObtenerNombreOpcion()
         {
             string txt = nroOrden + ". " + nombre;
             return txt;
         }
 
+        /// <summary> Obtiene las validaciones requeridas para la opcion </summary>
         public Validacion[] GetValidaciones()
         {
             return validacionesRequeridas;
