710331a0a900ca100c8a5258123ce1513317f7e8
LucsCarl <LucsCarl@broMomento>
2023-06-02 13:15:18 -0300

    Comentarios estado

--- Entidades/Estado.cs
+++ Entidades/Estado.cs
@@ -15,21 +15,25 @@ namespace PPAI_IVR_2023.Entidades
             this.nombre = nombre;
         }
 
+        /// <summary> Obtiene el nombre del estado </summary>
         public string GetNombre()
         {
             return nombre;
         }
 
+        /// <summary> Comprueba que sea el estado "Iniciada" </summary>
         public bool EsIniciada()
         {
             return this.GetNombre() == "Iniciada";
         }
 
+        /// <summary> Comprueba que sea el estado "En Curso" </summary>
         public bool EsEnCurso()
         {
             return this.GetNombre() == "En Curso";
         }
 
+        /// <summary> Comprueba que sea el estado "Finalizada" </summary>
         public bool EsFinalizada()
         {
             return this.GetNombre() == "Finalizada";
